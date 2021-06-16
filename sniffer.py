d7e42e24278379c6a6721f4ad85315c8198cb6e0
Daniël Goossens <daniel@goossens.ch>
2021-06-16 14:37:02 +0200

    Add argparse for input arguments and set up proper packet filtering

--- sniffer.py
+++ sniffer.py
@@ -1,7 +1,24 @@
 from scapy.all import *
+from scapy.layers.http import *
+import sys
+import argparse
 
-def print_pkt(pkt): 
-    pkt.show() 
+parser = argparse.ArgumentParser(description="Owncast Packet Interceptor")
+parser.add_argument("-dst", default="191.168.56.103")
+args = parser.parse_args()
+host = args.dst
 
-pkt = sniff(filter="length == 379", prn=print_pkt)
-#and tcp and host 191.168.56.103
+def print_pkt(pkt):
+    if not pkt.haslayer(HTTPRequest):
+        return
+            
+    http=pkt[HTTPRequest]
+    path=http.Path.decode()
+    print(f"Got packet with path {path}")
+    
+    if not 'stream.m3u8' in path:
+        return
+    
+    pkt.show()
+
+pkt = sniff(filter="dst host " + host, prn=print_pkt)
