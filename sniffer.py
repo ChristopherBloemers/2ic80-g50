cf4d551e04da6f60c43e78cac0398fd4363e7471
Daniël Goossens <daniel@goossens.ch>
2021-06-16 15:00:03 +0200

    Add args for the fake port and support custom ports for the main target owncast

--- sniffer.py
+++ sniffer.py
@@ -5,15 +5,34 @@ import argparse
 
 parser = argparse.ArgumentParser(description="Owncast Packet Interceptor")
 parser.add_argument("-dst", default="191.168.56.103")
+parser.add_argument("-port", default="8080")
+parser.add_argument("-fake", default="")
+parser.add_argument("-fake_port", default="")
 args = parser.parse_args()
+
 host = args.dst
+port = args.port
+fake = args.fake
+fake_port = args.fake_port
+
+# By default we use the destination (the real owncast) as the fake host but assume the port is different, this is true in our testing model
+if fake == "":
+    fake = host
+if fake_port == "":
+    fake_port = port
+
+print(f"Binding port {port} as an HTTP connection port")
+bind_layers(TCP, HTTP, dport=int(port))
+bind_layers(TCP, HTTP, sport=int(port))
+
+print(f"Redirecting stream on {host}:{port} to {fake}:{fake_port}")
 
 def print_pkt(pkt):
     if not pkt.haslayer(HTTPRequest):
         return
             
-    http=pkt[HTTPRequest]
-    path=http.Path.decode()
+    http = pkt[HTTPRequest]
+    path = http.Path.decode()
     print(f"Got packet with path {path}")
     
     if not 'stream.m3u8' in path:
@@ -21,4 +40,6 @@ def print_pkt(pkt):
     
     pkt.show()
 
-pkt = sniff(filter="dst host " + host, prn=print_pkt)
+filter = "dst host " + host + " and dst port " + port
+print(f"Filtering packets on {filter}")
+pkt = sniff(filter=filter, prn=print_pkt)
